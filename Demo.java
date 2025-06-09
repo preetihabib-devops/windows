e5d531a5f33b7e1faab11b3b81f01bbc1e13ed2a
preetihabib-devops <preetihabib06@gmail.com>
2025-06-09 10:24:45 +0530

    huu

---
+++ Demo.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } else {
+            System.out.println(number + " is Odd.");
+        }
+
+        scanner.close();
+    }
+}
